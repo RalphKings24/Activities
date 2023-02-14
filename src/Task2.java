a3731bf0ec20c60bcf0ecd5c14404172e4998e08
Rafael Reyes <rrs24.hw@gmail.com>
2023-02-14 15:21:31 -0500

    New Changes

--- src/Task2.java
+++ src/Task2.java
@@ -1,26 +1,20 @@
 /*
-"in1To10"
-Given a number n, return true if n is in the range 1..10, inclusive. Unless outsideMode is
-true, in which case return true if the number is less or equal to 1, or greater or equal to 10.
+"twoAsOne"
+Given three ints, a b c, return true if it is possible to add two of the ints to get the third.
 */
-
-import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
 
-        Scanner consola1 = new Scanner(System.in);
+        int a = 1;
+        int b = 2;
+        int c = 3;
 
-        System.out.println("Enter a int number = ");
-
-        int a = Integer.parseInt(consola1.nextLine());
-
-        if(a >= 1 && a < 10  || a >= 10 && a <= 1){
+        if(a + b == c || b + c == a || a + c == b){
             System.out.println("True");
         }
         else{
             System.out.println("False");
         }
-
     }
 }
--- src/Task3.java
+++ src/Task3.java
@@ -1,5 +1,31 @@
+/*
+"blueTicket"
+
+You have a blue lottery ticket, with ints a, b, and c on it. This makes three pairs,
+which we'll call ab, bc, and ac. Consider the sum of the numbers in each pair.
+If any pair sums to exactly 10, the result is 10. Otherwise if the ab sum is exactly 10 more
+than either bc or ac sums, the result is 5. Otherwise the result is 0.
+
+*/
+
 public class Task3 {
     public static void main(String[] args) {
+        int a = 9;
+        int b = 1;
+        int c = 0;
 
+        int d = a + b;
+        int e = b + c;
+        int f = a + c;
+
+        if(d == 10 || e == 10 || f == 10 ){
+            System.out.println("Resultado = 10");
+        }
+        else if(d == (e + 10) || d == (f + 10)){
+            System.out.println("Resultado = 5");
+        }
+        else{
+            System.out.println("Resultado = 0");
+        }
     }
 }
